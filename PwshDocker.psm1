4288ab985bad8d23899ff5bbf7c0b9c0b4f849f4
unknown <mlevan1992@gmail.com>
2019-07-09 14:04:40 -0400

    initial commit for docker image pull down and to check if docker exists

---
+++ PwshDocker.psm1
@@ -0,0 +1,66 @@
+function Get-DockerImage {
+    [cmdletbinding(DefaultParameterSetName = 'PullDockerImage', ConfirmImpact = 'low')]
+    param(
+        [parameter(Position = 0,
+            Mandatory,
+            HelpMessage = 'Please enter the docker image you wish to pull down to your local machine',
+            ValueFromPipeline = $true,
+            ParameterSetName = 'PullDockerImage')]
+        [ValidateNotNullOrEmpty()]
+        [alias('DockerImage')]
+        [string]$image
+    )
+
+    begin {
+        $getVersion = docker --version
+        if (-not ($getVersion)) {
+            Write-Warning 'Docker is not installed'
+            Write-Output 'Would you like to be directed to where you can download Docker?'
+            $download = Read-Host 'Type 1 for yes or 2 for no'
+
+            switch ($download) {
+            
+                "1" { 
+                    if ($IsWindows -like "*True*") {
+                        start microsoft-edge:https://docs.docker.com/docker-for-windows/install/
+                        Pause
+                        exit
+                    }
+                }
+                "2" {
+                    if ($IsMacOS -like "*True*") {
+                        start-process -FilePath '/Applications/Safari.app' -ArgumentList 'https://docs.docker.com/docker-for-mac/install/'
+                        Pause
+                        exit
+                    } 
+                }            
+            }
+        }
+    }
+
+    process { 
+        if ($PSBoundParameters.ContainsKey('image')) {
+            if ($image -like "*microsoft*") {
+                Write-Output 'Pulling down Windows image'
+                docker pull $image
+            }
+
+            if ($image -like "*ubuntu*") {
+                Write-Output "Pulling down Ubuntu image"
+                docker pull $image
+            }
+
+            if ($image -like "*centos*") {
+                Write-Output "Pulling down Centos image"
+                docker pull $image
+            }
+
+            else {
+                Write-Output 'Pulling down Docker image'
+                docker pull $image
+            }
+        }
+    }
+
+    end { }
+}
