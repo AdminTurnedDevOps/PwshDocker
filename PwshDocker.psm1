5a1cbf5dac9620a567fb2ffe85a5f1a6d2d657cd
Michael Levan <mlevan1992@outlook.com>
2019-09-28 14:14:43 -0400

    Updated Get-DockerImage function

--- PwshDocker.psm1
+++ PwshDocker.psm1
@@ -10,7 +10,32 @@ function Get-DockerImage {
         [ValidateNotNullOrEmpty()]
         [string]$dockerImage
     )
-    begin { }
+    begin {
+        $getVersion = docker --version
+        if (-not ($getVersion)) {
+            Write-Warning 'Docker is not installed'
+            Write-Output 'Would you like to be directed to where you can download Docker?'
+            $download = Read-Host 'Type 1 for yes or 2 for no'
+
+            switch ($download) {
+            
+                "1" { 
+                    if ($IsWindows -like "*True*") {
+                        start microsoft-edge:https://docs.docker.com/docker-for-windows/install/
+                        Pause
+                        exit
+                    }
+                }
+                "2" {
+                    if ($IsMacOS -like "*True*") {
+                        start-process -FilePath '/Applications/Safari.app' -ArgumentList 'https://docs.docker.com/docker-for-mac/install/'
+                        Pause
+                        exit
+                    } 
+                }            
+            }
+        }
+     }
     process { docker image ls $dockerImage }
     end { }
    
